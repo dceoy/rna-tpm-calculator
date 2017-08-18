3de24fc33bb2cc0176807ba9318de6ad5a61e201
dceoy <d.narsil@gmail.com>
2017-08-18 18:53:56 +0900

    Fix pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -30,11 +30,10 @@ set -e
 [[ "${1}" = '--debug' ]] && set -x && shift 1
 
 REPO_NAME='rna-tpm-calculator'
-SCRIPT_NAME='pipeline.sh'
-SCRIPT_VERSION='v0.0.1'
-SCRIPT_ROOT="$(dirname ${0})"
-SCRIPT_PATH="${SCRIPT_ROOT}/$(basename ${0})"
-DC="docker-compose -f ${SCRIPT_ROOT}/docker-compose.yml"
+REPO_VERSION='v0.0.1'
+REPO_ROOT="$(dirname ${0})"
+REPO_PATH="${REPO_ROOT}/$(basename ${0})"
+DC="docker-compose -f ${REPO_ROOT}/docker-compose.yml"
 DC_RUN="${DC} run --rm -u $(id -u):$(id -g)"
 INPUT_DIR="$(pwd)/input"
 OUTPUT_DIR="$(pwd)/output"
@@ -44,15 +43,15 @@ RUN_FLAG=0
 NO_QC=0
 
 function print_version {
-  echo "${REPO_NAME}: ${SCRIPT_NAME} ${SCRIPT_VERSION}"
+  echo "${REPO_NAME}: ${REPO_VERSION}"
 }
 
 function print_usage {
-  sed -ne '1,2d; /^#/!q; s/^#$/# /; s/^# //p;' ${SCRIPT_PATH}
+  sed -ne '1,2d; /^#/!q; s/^#$/# /; s/^# //p;' ${REPO_PATH}
 }
 
 function abort {
-  echo " ${SCRIPT_NAME}: ${*}" >&2
+  echo " ${REPO_NAME}: ${*}" >&2
   exit 1
 }
 
@@ -113,7 +112,6 @@ fi
 
 set -u
 PIGZ="pigz -p ${N_THREAD}"
-UNPIGZ="unpigz -p ${N_THREAD}"
 
 ### 1.  Preparation
 echo ">>> prepare output directories
@@ -121,25 +119,19 @@ echo ">>> prepare output directories
   - sample
   - ref
   - summary"
-SAMPLE_DIR="${OUTPUT_DIR}/sample"
-REF_DIR="${OUTPUT_DIR}/ref"
-SUMMARY_DIR="${OUTPUT_DIR}/summary"
-ls ${INPUT_DIR} | xargs -I {} bash -c "\
-  [[ -d ${SAMPLE_DIR}/{} ]] || mkdir -p ${SAMPLE_DIR}/{}
-"
-[[ -d "${REF_DIR}" ]] || mkdir ${REF_DIR}
-[[ -d "${SUMMARY_DIR}" ]] || mkdir ${SUMMARY_DIR}
+SAMPLE_DIR="${OUTPUT_DIR}/sample" && [[ -d "${SAMPLE_DIR}" ]] || mkdir -p ${SAMPLE_DIR}
+REF_DIR="${OUTPUT_DIR}/ref" && [[ -d "${REF_DIR}" ]] || mkdir ${REF_DIR}
+SUMMARY_DIR="${OUTPUT_DIR}/summary" && [[ -d "${SUMMARY_DIR}" ]] || mkdir ${SUMMARY_DIR}
+ls ${INPUT_DIR} | xargs -I {} bash -c "[[ -d ${SAMPLE_DIR}/{} ]] || mkdir ${SAMPLE_DIR}/{}"
 echo
 
 echo '>>> print software versions'
-VERSIONS_TXT="${OUTPUT_DIR}/versions.txt"
-print_version | tee ${VERSIONS_TXT}
-for l in 'fastqc' 'prinseq' 'rsem'; do
-  echo -n "${l}: " | tee -a ${VERSIONS_TXT}
-  ${DC_RUN} ${l} --version | tee -a ${VERSIONS_TXT}
-done
-echo -n "bowtie2: " | tee -a ${VERSIONS_TXT}
-${DC_RUN} --entrypoint bowtie2 rsem --version | tee -a ${VERSIONS_TXT}
+VER_TXT="${OUTPUT_DIR}/versions.txt"
+echo "[${REPO_NAME}]" | tee ${VER_TXT} && print_version | tee -a ${VER_TXT}
+echo '[fastqc]' | tee -a ${VER_TXT} && ${DC_RUN} fastqc --version | tee -a ${VER_TXT}
+echo '[prinseq]' | tee -a ${VER_TXT} && ${DC_RUN} prinseq --version | tee -a ${VER_TXT}
+echo '[bowtie2]' | tee -a ${VER_TXT} && ${DC_RUN} bowtie2 --version | tee -a ${VER_TXT}
+echo '[rsem]' | tee -a ${VER_TXT} && ${DC_RUN} rsem --version | tee -a ${VER_TXT}
 echo
 
 REF_TAG="$(basename ${INPUT_REF_FASTA} | awk -F '.fasta' '{print $1}')"
@@ -147,16 +139,16 @@ RSEM_PREP_LOG="${REF_DIR}/rsem_prepare_reference.log"
 if [[ ! -f ${RSEM_PREP_LOG} ]]; then
   echo '>>> prepare RSEM reference files'
   mkdir "${REF_DIR}/rsem"
-  ${UNPIGZ} -c ${INPUT_REF_FASTA} > ${REF_DIR}/${REF_TAG}.fasta
-  ${UNPIGZ} -c ${INPUT_REF_GENE_MAP_TXT} > ${REF_DIR}/${REF_TAG}_gene_map.txt
-  ${DC_RUN} -v ${REF_DIR}:/ref:ro -v ${REF_DIR}/rsem:/ref_rsem \
+  ${PIGZ} -dc ${INPUT_REF_FASTA} > ${REF_DIR}/${REF_TAG}.fasta
+  ${PIGZ} -dc ${INPUT_REF_GENE_MAP_TXT} > ${REF_DIR}/${REF_TAG}_gene_map.txt
+  ${DC_RUN} -v ${REF_DIR}:/rf:ro -v ${REF_DIR}/rsem:/rr \
     --entrypoint rsem-prepare-reference \
     rsem \
     -p ${N_THREAD} \
     --bowtie2 \
-    --transcript-to-gene-map /ref/${REF_TAG}_gene_map.txt \
-    /ref/${REF_TAG}.fasta \
-    /ref_rsem/${REF_TAG} \
+    --transcript-to-gene-map /rf/${REF_TAG}_gene_map.txt \
+    /rf/${REF_TAG}.fasta \
+    /rr/${REF_TAG} \
     > ${RSEM_PREP_LOG} 2>&1
   ${PIGZ} ${REF_DIR}/${REF_TAG}.fasta ${REF_DIR}/${REF_TAG}_gene_map.txt
   echo
@@ -166,53 +158,50 @@ fi
 ### 2.  QC checks
 if [[ ${NO_QC} -eq 0 ]]; then
   echo '>>> execute QC checks using FastQC'
-  ls ${SAMPLE_DIR} | xargs -I {} bash -c "\
-    ${DC_RUN} -v ${SAMPLE_DIR}/{}:/output \
-    fastqc \
-    --threads ${N_THREAD} \
-    --nogroup \
-    --outdir /output \
-    /output/raw.fastq.gz \
-    > ${SAMPLE_DIR}/{}/fastqc.log 2>&1
-  "
+  for s in $(ls ${SAMPLE_DIR}); do
+    ${DC_RUN} -v ${SAMPLE_DIR}/${s}:/sc \
+      fastqc \
+      --threads ${N_THREAD} \
+      --nogroup \
+      --outdir /sc \
+      /sc/raw.fastq.gz \
+      > ${SAMPLE_DIR}/${s}/fastqc.log 2>&1
+  done
   echo
 fi
 
 
 ### 3.  Trimming and filtering
 echo '>>> trim and filter sequences'
-find ${SAMPLE_DIR} -maxdepth 2 -type f -name 'raw.fastq.gz' | xargs ${UNPIGZ}
-ls ${SAMPLE_DIR} | xargs -P ${N_THREAD} -I {} bash -c "\
-  ${DC_RUN} -v ${SAMPLE_DIR}/{}:/output \
+${DC_RUN} -v ${SAMPLE_DIR}:/sd --entrypoint bash \
   prinseq \
-  -min_len 30 \
-  -trim_tail_right 5 \
-  -trim_tail_left 5 \
-  -min_qual_mean 20 \
-  -trim_qual_right 20 \
-  -trim_qual_left 20 \
-  -fastq output/raw.fastq \
-  -out_good output/prinseq_good \
-  -out_bad output/prinseq_bad \
-  > ${SAMPLE_DIR}/{}/prinseq_lite.log 2>&1
-"
-find ${SAMPLE_DIR} -maxdepth 2 -type f -name '*.fastq' | xargs ${PIGZ}
+  -c "\
+  ls /sd | xargs -P ${N_THREAD} -I {} bash -c '\
+  gzip -dc /sd/{}/raw.fastq.gz | /usr/local/bin/perl \
+  /usr/local/src/prinseq/prinseq-lite.pl \
+  -min_len 30 -trim_tail_right 5 -trim_tail_left 5 \
+  -min_qual_mean 20 -trim_qual_right 20 -trim_qual_left 20 \
+  -fastq stdin \
+  -out_good /sd/{}/prinseq_good -out_bad /sd/{}/prinseq_bad \
+  > /sd/{}/prinseq_lite.log 2>&1'"
+find ${SAMPLE_DIR} -name 'prinseq_raw.fastq' -or -name 'prinseq_bad.fastq' | xargs ${PIGZ}
 echo
 
 
 ### 4.  Mapping
 echo '>>> map reads and calculate TPM'
-ls ${SAMPLE_DIR} | xargs -I {} bash -c "\
-  ${DC_RUN} -v ${SAMPLE_DIR}/{}:/output -v ${REF_DIR}/rsem:/ref_rsem \
-  --entrypoint rsem-calculate-expression \
-  rsem \
-  --bowtie2 \
-  --estimate-rspd \
-  -p ${N_THREAD} \
-  --calc-ci \
-  /output/prinseq_good.fastq.gz \
-  /ref_rsem/${REF_TAG} \
-  /output/rsem_${REF_TAG}_{} \
-  > ${SAMPLE_DIR}/{}/rsem_calculate_expression.log 2>&1
-"
+for s in $(ls ${SAMPLE_DIR}); do
+  ${DC_RUN} -v ${SAMPLE_DIR}/${s}:/sc -v ${REF_DIR}/rsem:/rr \
+    --entrypoint rsem-calculate-expression \
+    rsem \
+    --bowtie2 \
+    --estimate-rspd \
+    -p ${N_THREAD} \
+    --calc-ci \
+    /sc/prinseq_good.fastq \
+    /rr/${REF_TAG} \
+    /sc/rsem_${REF_TAG}_${s} \
+    > ${SAMPLE_DIR}/${s}/rsem_calculate_expression.log 2>&1
+done
+find ${SAMPLE_DIR} -name 'prinseq_good.fastq' | xargs ${PIGZ}
 echo
